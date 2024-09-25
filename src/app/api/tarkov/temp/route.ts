929c24b286b7b16586509c5e2e1b378963c339c8
8auu <logancross02@me.com>
2024-09-25 19:49:15 +0100

    f

--- src/app/api/tarkov/temp/route.ts
+++ src/app/api/tarkov/temp/route.ts
@@ -23,7 +23,7 @@ export async function GET() {
               currency
               priceRUB
               vendor {
-                normalizedName
+                name
               }
             }
           }
