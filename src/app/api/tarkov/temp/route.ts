1c1d38424abd720c401c0ecb552d8d8a3f93c632
8auu <logancross02@me.com>
2024-09-25 19:34:39 +0100

    temp endpoint for debugging graphql production error :(

---
+++ src/app/api/tarkov/temp/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse } from "next/server";
+
+export const maxDuration = 30;
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  const query = `
+    {
+      traders {
+        name
+        cashOffers {
+          buyLimit
+          item {
+            id
+            name
+            types
+            basePrice
+            lastLowPrice
+            avg24hPrice
+            lastOfferCount
+            inspectImageLink
+            sellFor {
+              currency
+              priceRUB
+              vendor {
+                name
+              }
+            }
+          }
+          currency
+          price
+          minTraderLevel
+          buyLimit
+        }
+      }
+    }
+  `;
+
+  const response = await fetch("https://api.tarkov.dev/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch prices ${response.status}, ${response.statusText}`,
+    );
+  }
+
+  const data = await response.json();
+  console.log("TRADER1", response.status, response.statusText);
+  console.log("TRADER1", JSON.stringify(data.errors, null, 2));
+  console.log("TRADER1", data);
+  return NextResponse.json(data, { status: 200 });
+}
